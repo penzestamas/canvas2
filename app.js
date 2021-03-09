8738444c24513092f8fbde3840ed49f4e2939074
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-09 09:11:08 +0100

    js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var context = canvas.getContext('2d'); //rajzolási kontextus (lehet 3d is)
 context.beginPath();
 context.moveTo(55,70); //-től
 context.lineTo(150,100); //-ig
+context.strokeStyle = 'orange'; //körvonal színe (stroke() előtt kell lennie!!)
 context.stroke(); //körvonal
-context.strokeStyle = 'orange'; //körvonal színe
 context.fill(); //kitöltés
